864bd259918093bc4daf6980cbce911435561583
Gin <eriegin96@gmail.om>
2023-07-17 17:08:24 +0700

    feat: flash sale count down

--- src/app/(site)/_components/FlashSale/index.tsx
+++ src/app/(site)/_components/FlashSale/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./flashSale.module.scss";
 import Carousel from "../Carousel";
 import ProductCard from "../ProductCard";
@@ -5,11 +7,36 @@ import {setting} from "@/constants/carouselSetting";
 import {productList} from "../../../../mock/data";
 import {IconFlashSale} from "@/assets";
 import Image from "next/image";
+import {CountdownProps, Statistic} from "antd";
+import dayjs from "dayjs";
+import {useState} from "react";
+import duration from "dayjs/plugin/duration";
 
-type Props = {};
+dayjs.extend(duration);
+
+const {Countdown} = Statistic;
 const carouselSetting = {...setting, autoplaySpeed: 3500};
+const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2; // Dayjs is also OK;
 
-export default function FlashSale({}: Props) {
+export default function FlashSale() {
+	const [countDown, setCountDown] = useState({
+		day: "",
+		hour: "",
+		minute: "",
+		second: "",
+	});
+
+	const handleChange: CountdownProps["onChange"] = (value) => {
+		if (typeof value === "number") {
+			setCountDown({
+				day: dayjs.duration(value).format("DD"),
+				hour: dayjs.duration(value).format("HH"),
+				minute: dayjs.duration(value).format("mm"),
+				second: dayjs.duration(value).format("ss"),
+			});
+		}
+	};
+
 	return (
 		<div>
 			<div className={styles.title}>
@@ -19,7 +46,14 @@ export default function FlashSale({}: Props) {
 					<span className={styles.afterIcon}>ash sale</span>
 				</div>
 				<div className={styles.clock}>
-					<span>07</span>:<span>48</span>:<span>25</span>
+					{/* Hide antd countdown */}
+					<Countdown
+						value={deadline}
+						onChange={handleChange}
+						style={{display: "none"}}
+					/>
+					<span>{countDown.day}</span>:<span>{countDown.hour}</span>:
+					<span>{countDown.minute}</span>:<span>{countDown.second}</span>
 				</div>
 			</div>
 
