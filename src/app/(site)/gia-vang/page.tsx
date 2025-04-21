d837aa8403de7dee2419854915695516ca8b1076
Gin <eriegin96@gmail.om>
2025-04-21 11:20:35 +0700

    feat: change clock in gia-vang

--- src/app/(site)/gia-vang/page.tsx
+++ src/app/(site)/gia-vang/page.tsx
@@ -4,9 +4,20 @@ import {Table} from "antd";
 import {TradingViewWidget} from "../_components";
 import styles from "./giaVang.module.scss";
 import {goldColumns, goldData, TGoldTable} from "./static";
-import {formatDate, formatFulldate} from "@/utils/formatDate";
+import {formatFulldate} from "@/utils/formatDate";
+import {useEffect, useState} from "react";
 
 export default function Page() {
+	const [date, setDate] = useState(new Date());
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDate(new Date());
+		}, 1000 * 60);
+
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.titleWrapper}>
@@ -14,7 +25,7 @@ export default function Page() {
 				<h4 className={styles.text}>MỸ THỚI, CÁI SAO, LX, AG</h4>
 				<h4 className={styles.text}>KÍNH CHÀO QUÝ KHÁCH</h4>
 				<h4 className={styles.text}>TCCS 01:2024/T.TH</h4>
-				<div className={styles.date}>{formatFulldate(new Date())}</div>
+				<div className={styles.date}>{formatFulldate(date)}</div>
 			</div>
 			<div className={styles.tableWrapper}>
 				<Table<TGoldTable>
